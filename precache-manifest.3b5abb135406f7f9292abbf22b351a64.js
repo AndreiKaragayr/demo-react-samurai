4742bd0a0fb7aaf2de958342078774b8ad2339c4
andreikara404 <andreikara404@gmail.com>
2021-01-08 13:37:46 +0200

    Updates

---
+++ precache-manifest.3b5abb135406f7f9292abbf22b351a64.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "984f5315e62c1c9980ff78a02c639a49",
+    "url": "/demo-react-samurai/index.html"
+  },
+  {
+    "revision": "0c056aba55b2a8bb0dff",
+    "url": "/demo-react-samurai/static/css/3.70dd8895.chunk.css"
+  },
+  {
+    "revision": "ac13f60ea3e8cd994fdf",
+    "url": "/demo-react-samurai/static/css/4.63658a30.chunk.css"
+  },
+  {
+    "revision": "bfb248210057986e27b7",
+    "url": "/demo-react-samurai/static/css/main.651db066.chunk.css"
+  },
+  {
+    "revision": "6f00dca17df3a8afca92",
+    "url": "/demo-react-samurai/static/js/2.86c3fd42.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/demo-react-samurai/static/js/2.86c3fd42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c056aba55b2a8bb0dff",
+    "url": "/demo-react-samurai/static/js/3.586eb064.chunk.js"
+  },
+  {
+    "revision": "ac13f60ea3e8cd994fdf",
+    "url": "/demo-react-samurai/static/js/4.832a704f.chunk.js"
+  },
+  {
+    "revision": "bfb248210057986e27b7",
+    "url": "/demo-react-samurai/static/js/main.68a7d4cc.chunk.js"
+  },
+  {
+    "revision": "1fd491e12c626eee38b4",
+    "url": "/demo-react-samurai/static/js/runtime-main.7141c00c.js"
+  },
+  {
+    "revision": "8a30b7720b224b57fd7e9f8dc840671f",
+    "url": "/demo-react-samurai/static/media/avatar.8a30b772.jpg"
+  },
+  {
+    "revision": "beaed651af8d5042c592692ba32967c3",
+    "url": "/demo-react-samurai/static/media/empty-img.beaed651.svg"
+  },
+  {
+    "revision": "4beefb16b6100e7d30922420dd642bb6",
+    "url": "/demo-react-samurai/static/media/lading.4beefb16.svg"
+  },
+  {
+    "revision": "d5013f6cbc01832e2d2daacb3c962b10",
+    "url": "/demo-react-samurai/static/media/no_avatar.d5013f6c.jpg"
+  }
+]);
